8a523a604080b60357e4afb538d406087dc9427b
Davide Bucher <>
2024-01-24 11:47:23 +0100

    add specific track args, update README, add requirements.txt

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
 # tftmixer
 
-usage: 
-python3 main.py --arg1 --arg2 --arg3...
+## Usage
 
-args:
-- all genres: --pentakill, --country, --disco, ...
-- enhancements: --maestro, --illbeats, --mixmaster, --jazz
+`python3 main.py --random`
+`python3 main.py --pogs` -> to be updated
+
+OR:
+`python3 main.py --arg1 --arg2 --arg3 ...`
+where args can be:
+- all genres: `--pentakill`, `--country`, `--disco`, ...
+- specific track: `--country_m`, `disco_d`, `pentakill_s`, ...
+    - add `_m` for main, `_d` for drums, `_s` for secondary
+- enhancements: `--maestro`, `--illbeats`, `--mixmaster`, `--jazz`
+- when: by default, it will use the early version of the songs. Adding `--late` to the args uses the lategame version of the songs
+
+## examples
+`python3 main.py --pentakill --kda_s`
+```
+['./tracks/pentakill_early_drums.ogg',
+ './tracks/pentakill_early_main.ogg',
+ './tracks/pentakill_early_secondary.ogg',
+ './tracks/kda_early_secondary.ogg']
+ ```
+
+`python3 main.py --country --pentakill_m --late`
+['./tracks/country_late_drums.ogg',
+ './tracks/country_late_main.ogg',
+ './tracks/pentakill_late_main.ogg']
+
 
 # dependencies
 pip3 install pydub
--- main.py
+++ main.py
@@ -5,9 +5,42 @@ from randomizer import Randomizer
 
 from pprint import pprint
 
-
-def args2tracks(args):
+def _get_predefined_genre(arg, predefined):
     tracks = []
+
+    match arg:
+        case "--country":
+            tracks += predefined.country
+        case "--pentakill":
+            tracks += predefined.pentakill
+        case "--heartsteel":
+            tracks += predefined.heartsteel
+        case "--truedamage":
+            tracks += predefined.truedamage
+        case "--kda":
+            tracks += predefined.kda
+        case "--disco":
+            tracks += predefined.disco
+        case "--8bit":
+            tracks += predefined.bit
+        case "--emo":
+            tracks += predefined.emo
+        case "--punk":
+            tracks += predefined.punk
+        case "--edm":
+            tracks += predefined.edm
+        case "--hyperpop":
+            tracks += predefined.hyperpop
+        case _:
+            pass
+        
+    return tracks
+
+def args_to_tracks(args):
+    tracks = []
+
+    if len(args) == 1:
+        args.append("--random")
 
     predefined = GenresEarly
     predefined_enhance = EnhancesEarly
@@ -15,39 +48,38 @@ def args2tracks(args):
         predefined = GenresLate
         predefined_enhance = EnhancesLate
 
-    # main genre
     for arg in args:
-        match arg:
-            case "--random":
-                tracks = Randomizer().getRandomMix()
-                continue
-            case "--pogs":
-                tracks = Pogs().getRandomPog()
-                continue
-            case "--country":
-                tracks += predefined.country
-            case "--pentakill":
-                tracks += predefined.pentakill
-            case "--heartsteel":
-                tracks += predefined.heartsteel
-            case "--truedamage":
-                tracks += predefined.truedamage
-            case "--kda":
-                tracks += predefined.kda
-            case "--disco":
-                tracks += predefined.disco
-            case "--8bit":
-                tracks += predefined.bit
-            case "--emo":
-                tracks += predefined.emo
-            case "--punk":
-                tracks += predefined.punk
-            case "--edm":
-                tracks += predefined.edm
-            case "--hyperpop":
-                tracks += predefined.hyperpop
-            case _:
-                pass
+
+        tmp_tracks = []
+
+        arg_array = arg.split("_")
+        genre = arg_array[0]
+
+        if arg == "--random":
+            tracks = Randomizer().getRandomMix()
+            continue
+        elif arg == "--pogs":
+            tracks = Pogs().getRandomPog()
+            continue
+        else:
+            tmp_tracks = _get_predefined_genre(genre, predefined)
+
+        # if arg has prefix of _m, _d, _s -> filter by main, drum or secondary
+        if len(arg_array) > 1:
+            match arg_array[1]:
+                case "m":
+                    # use _main only out of tmp_tracks
+                    tmp_tracks = [s for s in tmp_tracks if "_main" in s]
+                case "d":
+                    # use _drums only out of tmp_tracks
+                    tmp_tracks = [s for s in tmp_tracks if "_drums" in s]
+                case "s":
+                    # use _secondary only out of tmp_tracks
+                    tmp_tracks = [s for s in tmp_tracks if "_secondary" in s]
+                case "_":
+                    pass
+        tracks += tmp_tracks
+
     # add enhances
     for arg in args:
         match arg:
@@ -65,7 +97,7 @@ def args2tracks(args):
     pprint(tracks)
     return tracks
 
-tracks = args2tracks(sys.argv)
+tracks = args_to_tracks(sys.argv)
 
 player = Player(volume=0.5, frequency=44100, buffer=4096)
 player.overlay_multiple_ogg(tracks)
